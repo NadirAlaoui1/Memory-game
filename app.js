ceeb5357729879c735cf8ecc974ff3fad020a8e4
nadiralaoui1 <nadiralaoui23@gmail.com>
2019-06-24 14:32:15 +0200

    working on js

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 const cards = document.querySelectorAll('.memory-card')
-let hasFlippedcard = false
+
+let hasFlippedCard = false
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.add('flip')
+
+    if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+} else {
+    hasFlippedCard = false;
+    secondCard = this;
+
+    function checkForMatch() {
+    if (firstCard.dataset.framework ===
+        secondCard.dataset.framework)  {
+
+            
+        } else {
+            setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+        },1000);
+    }
+}
+
+function disableCards(){firstCard.removeEventListener('click', flipCard);
+secondCard.removeEventListener('click', flipCard);
+}
+
 cards.forEach(card => card.addEventListener('click', flipcard))
